1ed4dff4b41ccbf27f962a548116a4ffbe5a72c8
Ryan Wells <ryanwellsconsulting@gmail.com>
2023-04-16 20:33:16 -0700

    Finished ErrorBoundary. Deploying to GitHub.

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from "../components/ErrorBoundary";
+//import {robots} from "./robots";
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: '',
+    }
+  }
+
+  componentDidMount() {
+    //console.log('check');
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => this.setState({robots: users}));
+  }
+
+  onSearchChange = (event) => {
+    //console.log(event.target.value);
+    this.setState({searchfield: event.target.value})
+
+    //console.log(filteredRobots);
+  }
+
+  render() {
+    const {robots, searchfield} = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className='tc'>
+              <h1 className='f2'>RoboFriends</h1>
+              <SearchBox searchChange={this.onSearchChange}/>
+              <Scroll>
+                <ErrorBoundary><CardList robots={filteredRobots}/></ErrorBoundary>
+              </Scroll>
+            </div>
+        );
+  }
+}
+
+export default App;
